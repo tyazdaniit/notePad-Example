e473f761facc77837072fc6748a3996bfa0af4d4
T.Yazadni <56879548+yazdani1994@users.noreply.github.com>
2022-04-23 21:07:37 +0430

    add new events

--- js/app.js
+++ js/app.js
@@ -3,12 +3,19 @@ let inputEl = $.getElementById("input-field");
 let btnSaveEl = $.getElementById("btn-save");
 let listEl = $.getElementById("listed");
 let colorsBox = $.querySelectorAll(".color-box");
+let deleteBtn = $.querySelector("#btn-delete");
 
 colorsBox.forEach((colorsBox) => {
   colorsBox.addEventListener("click", () => {
     let mainColor = colorsBox.style.backgroundColor;
     inputEl.style.backgroundColor = mainColor;
   });
+});
+
+inputEl.addEventListener("keyup", (event) => {
+  if (event.keyCode == 13) {
+    btnSaveEl.onclick();
+  }
 });
 
 let noteCount = 1;
@@ -24,6 +31,7 @@ btnSaveEl.onclick = () => {
     let containerEl = $.createElement("div");
     containerEl.setAttribute("class", "card shadow-sm rounded");
     containerEl.setAttribute("id", "note" + noteCount);
+    containerEl.setAttribute("onClick", "removeNote(event)");
     containerEl.style.backgroundColor = color;
 
     let textEl = $.createElement("p");
@@ -38,3 +46,13 @@ btnSaveEl.onclick = () => {
     noteCount++;
   }
 };
+
+deleteBtn.addEventListener("click", () => {
+  inputEl.value = "";
+  inputEl.style.backgroundColor = "#fff";
+});
+
+function removeNote(event) {
+  let mainNote = event.target.parentNode;
+  mainNote.remove();
+}
